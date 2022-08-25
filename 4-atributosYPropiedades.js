316b537d56f909c46b888089dbfeaf68cebf9cf5
heokim <andresssshk@gmail.com>
2022-08-25 11:50:34 -0400

    Atributos y Propiedades de los Elementos

---
+++ 4-atributosYPropiedades.js
@@ -0,0 +1,7 @@
+// se puede acceder a cualquier atributo de un nodo con poner el nodo.[atributo]
+// este depende mucho del tipo de elemento, por eso es recomendable ver de la documentacion de Mozilla
+
+
+// la diferencia entre atributos y propiedades den HTML es que 
+// los atributos son usados para inicializar un elemento
+// y las propiedades son como variables que estan cambiando al usar la pagina
